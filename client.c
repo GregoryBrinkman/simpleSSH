d1b26151bc53b13a27f7cdc7f74bad1a0fcaa0a8
Greg <GregoryLucasBrinkman@siu.edu>
2016-09-18 18:26:59 -0500

    errorhandling and start of pseduoterminal

--- client.c
+++ client.c
@@ -1,3 +1,5 @@
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
@@ -7,71 +9,167 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <errno.h>
 
 #define PORT 4070
-#define BUFFSIZE 512
+#define BUFFSIZE 4096
 #define REMBASH "<rembash>\n"
-#define SECRET "cs407rembash\n"
+#define SECRET "<cs407rembash>\n"
 #define OK "<ok>\n"
 #define EXIT "exit\n"
 
+#define DEBUG
+
 void error(const char *);
+void sigchld_handler(int);
 
 int main(int argc, char *argv[])
 {
-  static char buff[BUFFSIZE];
-  int sockfd, len, result;
+  int sockfd, len;
   struct sockaddr_in address;
+  static char buff[BUFFSIZE];
 
-  if(argc < 2 || argc > 2)
-    error("USAGE: program ip-address");
+#ifdef DEBUG
+  int i;
+  i=1;
+  printf("%d\n", i++);
+#endif
+
+  if(argc != 2) error("USAGE: program IP-ADDRESS");
 
   char *tmp = argv[1];
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = inet_addr(tmp);
-  address.sin_port = htons(PORT);
-  len = sizeof(address);
-
-  result = connect(sockfd, (struct sockaddr *)&address, len);
-
-  if(-1 == result) error("Connection refused");
-
-  read(sockfd, &buff, strlen(REMBASH));
-  result = strncmp(buff, REMBASH, strlen(REMBASH));
-  if(0 != result) {
-    close(sockfd);
-    error("Rembash Protocol Incorrect");
+  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+    perror("Socket Call Failed");
+    exit(EXIT_FAILURE);
   }
 
-  write(sockfd, &SECRET, strlen(SECRET));
+  address.sin_family = AF_INET;
+  address.sin_port = htons(PORT);
+  inet_aton(tmp, &address.sin_addr);
+
+#ifdef DEBUG
+  printf("%d\n", i++);
+#endif
+
+  if(connect(sockfd, (struct sockaddr *)&address, sizeof(address)) == -1){
+    perror("Connection refused");
+    exit(EXIT_FAILURE);
+  }
+
+#ifdef DEBUG
+  printf("%d\n", i++);
+#endif
+
+
+  len = strlen(REMBASH);
+  if(read(sockfd, &buff, len) != len){
+    close(sockfd);
+    error("Read Failed");
+  }
+
+#ifdef DEBUG
+  printf("%d\n", i++);
+#endif
+
+
+  if(strcmp(buff, REMBASH) != 0){
+    close(sockfd);
+    error("Rembash Protocol Failed");
+  }
+
+#ifdef DEBUG
+  printf("%d\n", i++);
+#endif
+
+
+  len = strlen(SECRET);
+  if(write(sockfd, &SECRET, len) != len){
+    close(sockfd);
+    error("Secret Write Failed");
+  }
   read(sockfd, &buff, BUFFSIZE);
+
+#ifdef DEBUG
+  printf("%d\n", i++);
+#endif
+
 
   if(0 != strncmp(buff, OK, strlen(OK))){
     close(sockfd);
     error("Wrong Secret");
   }
 
+#ifdef DEBUG
+  printf("%d\n", i++);
+#endif
 
-  //child
-  if(fork() == 0){
-    len = read(0, &buff, BUFFSIZE);
-    while(write(sockfd, &buff, len)){
-      len = read(0, &buff, BUFFSIZE);
-    }
-    close(sockfd);
-    exit(0);
+
+  //signal handler
+  struct sigaction act;
+  act.sa_handler = sigchld_handler;
+  act.sa_flags = 0;
+  sigemptyset(&act.sa_mask);
+  if(sigaction(SIGCHLD, &act,NULL) == -1) {
+    perror("Signal Handler Construction Failed");
+    exit(EXIT_FAILURE);
   }
-  //parent
-  while(0 < (len = read(sockfd, &buff, BUFFSIZE))){
-    write(0, &buff, len);
+  
+
+#ifdef DEBUG
+  printf("%d\n", i++);
+#endif
+
+  int cpid, nwrite, total;
+  switch(cpid = fork()) {
+  case -1: //error
+    perror("Fork failed");
+    exit(EXIT_FAILURE);
+  case 0: //child: reader
+
+#ifdef DEBUG
+    printf("%d\n", i++);
+#endif
+
+    nwrite = 0;
+    while(nwrite != -1 && (len = read(0, &buff, BUFFSIZE)) > 0){
+      total = 0;
+      do{
+        if((nwrite = write(sockfd, buff+total, len-total)) == -1) break;
+      }while((total += nwrite) < len);
+    }
+    if(errno)
+      perror("Client: Error during read/write from stdin to shell");
+    else
+      fprintf(stderr, "Client: Connection closed prematurely");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+  //parent: writer
+  nwrite = 0;
+  while(nwrite != -1 && (len = read(sockfd, &buff, BUFFSIZE)) > 0){
+    total = 0;
+    do{
+      if((nwrite = write(1, buff+total, len-total)) == -1) break;
+    }while((total += nwrite) < len);
   }
   close(sockfd);
+  act.sa_handler = SIG_IGN;
+  if(sigaction(SIGCHLD, &act, NULL) == -1)
+    perror("Client: Error setting SIGCHLD to be ignored");
+  kill(cpid, SIGTERM);
+
   return 0;
 }
 
-void error(const char* err_msg){
+void error(const char* err_msg)
+{
   fprintf(stderr, "%s\n", err_msg);
   exit(1);
+}
+
+void sigchld_handler(int signal)
+{
+  wait(NULL);
+  exit(EXIT_FAILURE);
 }
--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
 #include <signal.h>
@@ -8,14 +9,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define _XOPEN_SOURCE 600
 #define PORT 4070
-#define BUFFSIZE 512
+#define BUFFSIZE 4096
 #define REMBASH "<rembash>\n"
-#define SECRET "cs407rembash\n"
+#define SECRET "<cs407rembash>\n"
 #define ERROR "<error>\n"
 #define OK "<ok>\n"
 
 void handle_client(int);
+void accepted_client(int);
 
 int main(int argc, char *argv[])
 {
@@ -25,8 +28,8 @@ int main(int argc, char *argv[])
   struct sockaddr_in server_address;
   struct sockaddr_in client_address;
 
-  if(-1 == (server_sockfd = socket(AF_INET, SOCK_STREAM, 0))){
-    perror("");
+  if((server_sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+    perror("Socket Failed");
     exit(1);
   }
   server_address.sin_family = AF_INET;
@@ -34,31 +37,29 @@ int main(int argc, char *argv[])
   server_address.sin_port = htons(PORT);
   server_len = sizeof(server_address);
 
-  result = bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
-  if(-1 == result){
+  if((result = bind(server_sockfd, (struct sockaddr *)&server_address, server_len)) == -1){
     perror("Bind refused");
     exit(1);
   }
 
-  if(0 != (result = listen(server_sockfd, 5))){
-    perror("");
+  if((result = listen(server_sockfd, 5)) != 0){
+    perror("Listen Failed");
     exit(1);
   }
   signal(SIGCHLD, SIG_IGN);
 
+  client_len = sizeof(client_address);
   while(1) {
-    client_len = sizeof(client_address);
     client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
-    if(-1 == client_sockfd){
-      perror("");
-      close(client_sockfd);
-    }else
-      handle_client(client_sockfd);
+    
+    if(client_sockfd != -1)
+      if(fork() == 0) handle_client(client_sockfd);
+    close(client_sockfd);
   }
 }
 
 void handle_client(int connect_fd){
-  char buff[BUFFSIZE];
+  static char buff[BUFFSIZE];
 
   write(connect_fd, &REMBASH, strlen(REMBASH));
   read(connect_fd, &buff, strlen(SECRET));
@@ -66,18 +67,35 @@ void handle_client(int connect_fd){
   if(0 != strncmp(buff, SECRET, strlen(SECRET))){
     write(connect_fd, &ERROR, strlen(ERROR));
     close(connect_fd);
-    return;
+    exit(EXIT_FAILURE);
   }
 
   write(connect_fd, &OK, strlen(OK));
+  accepted_client(connect_fd);
+}
 
-  //child
-  if(fork() == 0){
-    int i = 0;
-    while(i < 3) dup2(connect_fd, i++);
-    setsid();
-    execlp("bash", "bash", "--noediting", "-i", NULL);
+
+void accepted_client(int connect_fd)
+{
+  /* int i = 0; */
+  /* while(i < 3) dup2(connect_fd, i++); */
+  /* execlp("bash", "bash", NULL); */
+  int masterfd, slavefd;
+  char *slavename;
+
+  masterfd = posix_openpt(O_RDWR|O_NOCTTY); 
+
+  if (masterfd == -1 || grantpt(masterfd) == -1
+      || unlockpt(masterfd) == -1 || (slavename = ptsname(masterfd)) == NULL){
+    close(masterfd);
+    exit(EXIT_FAILURE);
   }
-  close(connect_fd);
-  return;
+  if(fork() == 0){
+    close(masterfd);
+    if(setsid() == -1)
+      exit(EXIT_FAILURE);
+    if((slavefd = open(slavename, O_RDWR)) < 0)
+      exit(EXIT_FAILURE);
+  }
+
 }
